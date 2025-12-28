5f7070c22d38385c430e74058844d88cb6d450fb
Wasif <wasifullah182@gmail.com>
2025-12-28 14:09:41 +0500

    Fix Prisma client generation for CI

--- backend/server/src/prisma/prisma.service.ts
+++ backend/server/src/prisma/prisma.service.ts
@@ -1,21 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { ConfigService } from '@nestjs/config';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(configService: ConfigService) {
-    const connectionString = configService.get<string>('DATABASE_URL');
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-    
-    super({ adapter });
-  }
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
 
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async enableShutdownHooks() {
+    this.$on('beforeExit', async () => {
+      await this.$disconnect();
+    });
   }
 
   async onModuleDestroy() {
